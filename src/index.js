d96404614b4b89966bb4f7e49d379357061b235d
sqlnice <472252510@qq.com>
2021-09-05 23:07:20 +0800

    feat：Render

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom';
 import Game from './game';
 import Jsx from './jsx';
+import Render from './render';
 // ========================================
 
 // ReactDOM.render(<Game />, document.getElementById('root'));
-ReactDOM.render(Jsx, document.getElementById('root'));
+// ReactDOM.render(Jsx, document.getElementById('root'));
+ReactDOM.render(<Render />, document.getElementById('root'));
