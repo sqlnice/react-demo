b96417c0111e7a696af765e4838d461a1448210a
sqlnice <472252510@qq.com>
2021-09-11 14:16:11 +0800

    feat：代码分割

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-
+import React, { Suspense } from 'react';
 // import Game from './1_game';
 // import Jsx from './2_jsx';
 // import Render from './3_render';
@@ -11,17 +11,35 @@ import ReactDOM from 'react-dom';
 // import Forms from './9_forms';
 // import LiftingStateUp from './10_liftingStateUp';
 // import CompositionVsInheritance from './11_compositionVsInheritance';
-import ThinkingInReact from './12_thinkingInReact';
+// import ThinkingInReact from './12_thinkingInReact';
+const ThinkingInReact = React.lazy(() => import('./12_thinkingInReact'));
 // ========================================
 
 // ReactDOM.render(<Game />, document.getElementById('root'));
+// JSX
 // ReactDOM.render(Jsx, document.getElementById('root'));
+// 元素渲染
 // ReactDOM.render(<Render />, document.getElementById('root'));
+// 组件和Props
 // ReactDOM.render(<ComponentsAndProps  />, document.getElementById('root'));
+// 事件处理
 // ReactDOM.render(<HandingEvent />, document.getElementById('root'));
+// 条件渲染
 // ReactDOM.render(<ConditionalRendering />, document.getElementById('root'));
+// 列表和Key
 // ReactDOM.render(<ListAndKeys numbers="[1, 2, 3, 4, 5]" />, document.getElementById('root'));
+// 表单
 // ReactDOM.render(<Forms />, document.getElementById('root'));
+// 状态提升
 // ReactDOM.render(<LiftingStateUp />, document.getElementById('root'));
+// 组合和继承
 // ReactDOM.render(<CompositionVsInheritance />, document.getElementById('root'));
-ReactDOM.render(<ThinkingInReact />, document.getElementById('root'));
+// React哲学
+// ReactDOM.render(<ThinkingInReact />, document.getElementById('root'));
+// 代码分割
+ReactDOM.render(
+  <Suspense fallback={<div>loading</div>}>
+    <ThinkingInReact />
+  </Suspense>,
+  document.getElementById('root')
+);
